70d4506981a5bbcc41054159b6c6f3ff3439d0d3
Stan O <spammyboy@protonmail.com>
2021-11-06 03:47:05 +0000

    v1.0
    - Rotating icon like a record
    - all 4 channels nailed
    - can reverse the music while isolating

--- main.lua
+++ main.lua
@@ -24,43 +24,106 @@ icon = lg.newImage("icon.png")
 
 file_path = "stems/3 follow god/"
 
-stem_1 = la.newSource(file_path.."1.wav", "stream")
-stem_2 = la.newSource(file_path.."2.wav", "stream")
-stem_3 = la.newSource(file_path.."3.wav", "stream")
-stem_4 = la.newSource(file_path.."4.wav", "stream")
+original_stems = {}
+reverse_stems = {}
+stems = original_stems
+stems_SD = {}
 
-stem_1:play()
-stem_2:play()
-stem_3:play()
-stem_4:play()
+
+for i = 1, 4 do
+    stems[i] = la.newSource(file_path..tostring(i)..".wav", "static")
+    stems_SD[i] = lsound.newSoundData(file_path..tostring(i)..".wav")
+
+    local n_samples = stems[i]:getDuration("samples")
+    local temp_sd = lsound.newSoundData(n_samples, stems_SD[i]:getSampleRate(), stems_SD[i]:getBitDepth(), stems_SD[i]:getChannelCount())
+    for j = 0, n_samples - 1 do
+        temp_sd:setSample(j, 1, stems_SD[i]:getSample(n_samples - 1 - j, 1))
+        temp_sd:setSample(j, 2, stems_SD[i]:getSample(n_samples - 1 - j, 2))
+    end
+    reverse_stems[i] = la.newSource(temp_sd)
+
+end
+
+for i = 1, 4 do
+    stems[i]:play()
+end
 
 joystick = lj.getJoysticks()[1]
 
-function love.love()
-end
+font = lg.newFont(80)
+timer_text = lg.newText(font,"")
+
+-- for later
+-- stems[1]:setFilter({type="highpass",volume=1,lowgain=.001})
+
 
 function love.draw()
-    lg.draw(icon, lg.getWidth() / 2 - 270)
+
+    -- rotation off for now but keeping it for fun to show to people
+    local length = stems[1]:tell()
+    if stems == reverse_stems then
+        length = stems[1]:getDuration() - length
+    end
+    local rotation = (length / 4) % (math.pi * 2)
+    -- rotation = 0
+
+    lg.draw(icon, lg:getWidth() / 2, lg:getHeight() / 2, rotation, 1, 1, icon:getWidth() / 2, icon:getHeight() / 2)
+
+    lg.draw(timer_text)
 end
 
+-- 4 is a very awkward number to for loop,
+-- because it makes it almost less readable to do a 4 loop
+-- for 4 things, so I will only do it when it's helpful.
 function love.update(dt)
 
-    button_1_down = joystick:isGamepadDown("y")
-    button_2_down = joystick:isGamepadDown("b")
-    button_3_down = joystick:isGamepadDown("a")
-    button_4_down = joystick:isGamepadDown("x")
+    local button_1_down = joystick:isGamepadDown("y")
+    local button_2_down = joystick:isGamepadDown("b")
+    local button_3_down = joystick:isGamepadDown("a")
+    local button_4_down = joystick:isGamepadDown("x")
+    local button_rs = joystick:isGamepadDown("rightshoulder")
+    local button_ls = joystick:isGamepadDown("leftshoulder")
+    local button_back = joystick:isGamepadDown("back")
 
     if button_1_down or button_2_down or button_3_down or button_4_down then
-        stem_1:setVolume(tovolume(button_1_down))
-        stem_2:setVolume(tovolume(button_2_down))
-        stem_3:setVolume(tovolume(button_3_down))
-        stem_4:setVolume(tovolume(button_4_down))
+        stems[1]:setVolume(tovolume(button_1_down))
+        stems[2]:setVolume(tovolume(button_2_down))
+        stems[3]:setVolume(tovolume(button_3_down))
+        stems[4]:setVolume(tovolume(button_4_down))
     else
-        stem_1:setVolume(1)
-        stem_2:setVolume(1)
-        stem_3:setVolume(1)
-        stem_4:setVolume(1)
+        stems[1]:setVolume(1)
+        stems[2]:setVolume(1)
+        stems[3]:setVolume(1)
+        stems[4]:setVolume(1)
     end
+
+    if button_rs then
+        if stems ~= reverse_stems then
+            stems = reverse_stems
+            for i = 1, 4 do
+                stems[i]:seek(original_stems[i]:getDuration() - original_stems[i]:tell())
+                original_stems[i]:pause()
+                stems[i]:play()
+            end
+        end
+    else
+        if stems ~= original_stems then
+            stems = original_stems
+            for i = 1, 4 do
+                stems[i]:seek(reverse_stems[i]:getDuration() - reverse_stems[i]:tell())
+                reverse_stems[i]:pause()
+                stems[i]:play()
+            end
+        end
+
+    end
+
+    local length = stems[1]:tell()
+    if stems == reverse_stems then
+        length = stems[1]:getDuration() - length
+    end
+
+    timer_text:set(string.format("%.2f", tostring(length)))
 
 end
 
